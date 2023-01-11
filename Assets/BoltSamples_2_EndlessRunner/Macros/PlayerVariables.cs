6be11ee54d3cbf785db90b32887610b5bcd18bcd
Nigel Morris <nigel.morris@gmail.com>
2023-01-11 12:47:02 +0000

    2022

---
+++ Assets/BoltSamples_2_EndlessRunner/Macros/PlayerVariables.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVariables : MonoBehaviour
+{
+    public float jumpStrength;
+
+
+
+}
---
+++ Assets/BoltSamples_2_EndlessRunner/Macros/scoreScript.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class scoreScript : MonoBehaviour
+{
+    public int score;
+
+    public scoreScript data;
+
+    
+
+}
